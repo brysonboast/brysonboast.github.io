309b1b177e74fab3668608c38ae550d92d994a24
brysonboast <91611594+brysonboast@users.noreply.github.com>
2023-08-24 18:04:58 -0400

    reset

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 
-const aElements = document.getElementsByClassName('tag');
 
-const resetClasses = function() {
-    for (el of aElements) {
-        el.className = 'closed tag';
-    }
-}
-
-for (el of aElements) {
-    el.addEventListener('click', (event) => {
-        resetClasses();
-        event.target.className = 'open tag';
-    })
-}
 
